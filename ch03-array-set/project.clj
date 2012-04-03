9414a03f6b5cd9f503f56274e58a2cebf85cbb9f
Chas Emerick <chas@cemerick.com>
2012-04-03 13:21:54 -0400

    make array-set runnable

--- ch03-array-set/project.clj
+++ ch03-array-set/project.clj
@@ -1,3 +1,7 @@
-(defproject com.clojurebook/array-set "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject com.clojurebook/array-set "1.0.0"
+  :description "An array-backed set implementation, analogous to
+Clojure's array-map, explored in chapter 3 of 'Clojure Programming' by
+Emerick, Carper, and Grand."
+  :url "http://github.com/clojurebook/ClojureProgramming"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta6"]]}})
